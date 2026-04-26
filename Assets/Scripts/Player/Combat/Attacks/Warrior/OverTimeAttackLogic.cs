f2b87bf762ed3e4c84df8514d1525749f9749953
Adam <krciladam@seznam.cz>
2026-04-26 18:48:32 +0200

    Add OverTimeAttackLogic for asynchronous damage application

---
+++ Assets/Scripts/Player/Combat/Attacks/Warrior/OverTimeAttackLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+    public static class OverTimeAttackLogic
+    {
+        public static async UniTask OverTimeAttackAsync(CancellationToken ct, float duration, float dmg, float interval)
+        {
+            float elapsedTime = 0f;
+            while (elapsedTime < duration)
+            {
+                ApplyDmg(dmg);
+                await UniTask.Delay(System.TimeSpan.FromSeconds(interval), cancellationToken: ct);
+                elapsedTime += interval;
+            }
+            Debug.Log("Spin attack completed.");
+        }
+
+        private static void ApplyDmg(float dmg)
+        {
+            Debug.Log("Applying dmg: " + dmg);
+        }
+    }
